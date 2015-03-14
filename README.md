5c09c92f737ea5a3b50833fa2c5c353c849aa344
kolesnikovakate <kolesnikovakate@gmail.com>
2015-03-15 00:26:38 +0300

    Урок 10 - UITableView

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS193P_Smashtag
-Developing iOS 8 Apps with Swift, Stanford CS193p
---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Екатерина Колесникова on 14.03.15.
+//  Copyright (c) 2015 kkate. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell
+{
+    var tweet: Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+
+    func updateUI() {
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        //tweetCreatedLabel?.text = nil
+
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil  {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+
+            tweetScreenNameLabel?.text = "\(tweet.user)"
+
+            if let profileImageURL = tweet.user.profileImageURL {
+                let qos = Int(QOS_CLASS_USER_INITIATED.value)
+                dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
+                    let imageData = NSData(contentsOfURL: profileImageURL)
+                    dispatch_async(dispatch_get_main_queue()) {
+                        if imageData != nil {
+                            self.tweetProfileImageView?.image = UIImage(data: imageData!)
+                        }
+                    }
+
+                }
+            }
+
+//            let formatter = NSDateFormatter()
+//            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+//                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+//            } else {
+//                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+//            }
+//            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+        }
+
+    }
+}
